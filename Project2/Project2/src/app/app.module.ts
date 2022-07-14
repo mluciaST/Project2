92e57772c3fb1ad0fd74c8ece9d290bfe941a3f1
twendell-skillstorm <twendell@skillstorm.com>
2022-07-14 15:38:28 -0400

    Added Login functionality
    Uses Google API for authentication and logging in and out

--- Project2/Project2/src/app/app.module.ts
+++ Project2/Project2/src/app/app.module.ts
@@ -1,5 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]

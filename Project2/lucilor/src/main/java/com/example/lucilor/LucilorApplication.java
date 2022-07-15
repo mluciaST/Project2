e894689d1152c9dd2e2b32740cc3cc16148a3fb3
mluciaST <106841357+mluciaST@users.noreply.github.com>
2022-07-15 14:02:36 -0400

    maven setup

---
+++ Project2/lucilor/src/main/java/com/example/lucilor/LucilorApplication.java
@@ -0,0 +1,13 @@
+package com.example.lucilor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucilorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucilorApplication.class, args);
+	}
+
+}
